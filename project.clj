80e0cf55174dff658d98c93d059543a99024e3b2
Kent OHASHI <ignorantia.juris.non.excusa@gmail.com>
2016-08-04 12:15:54 +0900

    add pomegranate

--- project.clj
+++ project.clj
@@ -5,5 +5,6 @@
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.8.0"]
                  [org.clojure/data.json "0.2.6"]
+                 [com.cemerick/pomegranate "0.3.1"]
                  [korma "0.4.2"]
                  [mysql/mysql-connector-java "5.1.39"]])
