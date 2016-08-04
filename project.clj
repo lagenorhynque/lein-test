5fb2fcce59fa0e4dd329cf046555d8bda0239da5
Kent OHASHI <ignorantia.juris.non.excusa@gmail.com>
2016-08-04 21:14:15 +0900

    add math.numeric-tower

--- project.clj
+++ project.clj
@@ -5,6 +5,7 @@
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.8.0"]
                  [org.clojure/data.json "0.2.6"]
+                 [org.clojure/math.numeric-tower "0.0.4"]
                  [com.cemerick/pomegranate "0.3.1"]
                  [korma "0.4.2"]
                  [mysql/mysql-connector-java "5.1.39"]])
