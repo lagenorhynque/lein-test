1fc86a207828c1edd40f5eb811ff34d4e488c03e
Kent OHASHI <ignorantia.juris.non.excusa@gmail.com>
2016-08-24 19:32:19 +0900

    add library dependencies

--- project.clj
+++ project.clj
@@ -4,9 +4,11 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.8.0"]
+                 [org.clojure/algo.monads "0.1.6"]
+                 [org.clojure/clojurescript "1.9.89"]
                  [org.clojure/data.json "0.2.6"]
                  [org.clojure/math.numeric-tower "0.0.4"]
-                 [org.clojure/clojurescript "1.9.89"]
+                 [org.clojure/test.check "0.9.0"]
                  [com.cemerick/pomegranate "0.3.1"]
                  [korma "0.4.2"]
                  [hiccup "1.0.5"]
