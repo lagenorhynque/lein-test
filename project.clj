19a397811d37b31ce3bf1681efab2935144221a7
Kent OHASHI <ignorantia.juris.non.excusa@gmail.com>
2016-08-25 12:35:28 +0900

    add incanter

--- project.clj
+++ project.clj
@@ -10,6 +10,7 @@
                  [org.clojure/math.numeric-tower "0.0.4"]
                  [org.clojure/test.check "0.9.0"]
                  [com.cemerick/pomegranate "0.3.1"]
+                 [incanter "1.5.7"]
                  [korma "0.4.2"]
                  [hiccup "1.0.5"]
                  [mysql/mysql-connector-java "5.1.39"]])
