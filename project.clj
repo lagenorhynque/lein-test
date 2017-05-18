3e136e81165f5c9c1e8c909053c6f7d45caacef9
Kent OHASHI <ignorantia.juris.non.excusa@gmail.com>
2017-05-18 19:54:09 +0900

    add dependencies

--- project.clj
+++ project.clj
@@ -7,15 +7,17 @@
                  [org.clojure/clojurescript "1.9.542"]
                  [org.clojure/algo.monads "0.1.6"]
                  [org.clojure/core.async "0.3.442"]
-                 [org.clojure/test.check "0.9.0"]]
- :plugins [[lein-cljsbuild "1.1.6"]]
- :source-paths ["src/clj" "test/clj"]
- :cljsbuild
- {:builds [{:source-paths ["src/cljs"]
-            :compiler {:output-to "resources/public/js/compiled/main.js"
-                       :optimizations :whitespace
-                       :pretty-print true}}]}
- :uberjar-name "lein-test.jar"
- :profiles
- {:uberjar {:aot :all
-            :main lein-test.core}})
+                 [org.clojure/test.check "0.9.0"]
+                 [funcool/cats "2.1.0"]
+                 [prismatic/schema "1.1.6"]]
+  :plugins [[lein-cljsbuild "1.1.6"]]
+  :source-paths ["src/clj" "test/clj"]
+  :cljsbuild
+  {:builds [{:source-paths ["src/cljs"]
+             :compiler {:output-to "resources/public/js/compiled/main.js"
+                        :optimizations :whitespace
+                        :pretty-print true}}]}
+  :uberjar-name "lein-test.jar"
+  :profiles
+  {:uberjar {:aot :all
+             :main lein-test.core}})
