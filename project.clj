014cdba102a4d4dc4d5ccee88a5f4a9f9f1803fa
Kent OHASHI <ignorantia.juris.non.excusa@gmail.com>
2017-09-19 14:41:46 +0900

    Bump up dependencies

--- project.clj
+++ project.clj
@@ -3,11 +3,12 @@
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.9.0-alpha20"]
+  :dependencies [[org.clojure/clojure "1.9.0-beta1"]
                  [org.clojure/clojurescript "1.9.854"]
                  [org.clojure/algo.monads "0.1.6"]
                  [org.clojure/core.async "0.3.443"]
                  [org.clojure/core.match "0.3.0-alpha4"]
+                 [org.clojure/core.typed "0.4.1"]
                  [org.clojure/test.check "0.9.0"]
                  [clj-time "0.14.0"]
                  [funcool/beicon "4.0.0"]
