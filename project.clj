20b6d937ed805d955682c391be2a8f4606b44d2d
Kent OHASHI <ignorantia.juris.non.excusa@gmail.com>
2017-11-28 14:02:01 +0900

    update dependencies

--- project.clj
+++ project.clj
@@ -3,13 +3,13 @@
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[clj-time "0.14.0"]
-                 [funcool/beicon "4.0.0"]
+  :dependencies [[clj-time "0.14.2"]
+                 [funcool/beicon "4.1.0"]
                  [funcool/cats "2.1.0"]
                  [org.clojure/algo.monads "0.1.6"]
-                 [org.clojure/clojure "1.9.0-beta3"]
+                 [org.clojure/clojure "1.9.0-RC2"]
                  [org.clojure/clojurescript "1.9.946"]
-                 [org.clojure/core.async "0.3.443"]
+                 [org.clojure/core.async "0.3.465"]
                  [org.clojure/core.match "0.3.0-alpha4"]
                  [org.clojure/test.check "0.9.0"]
                  [prismatic/schema "1.1.7"]]
